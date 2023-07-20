a63d7cf9ecd52c28eaf81a09d3a55191b62b1a33
mulwajoseph <mulwajose.jm@gmail.com>
2023-07-20 11:23:35 +0300

    modifying element lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Get the main element with id 'main'
+const mainElement = document.getElementById('main');
+
+// Remove the main element from the DOM
+mainElement.remove();
+
+// Create a new h1 element and set its text content
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+// Set the id attribute to 'victory'
+newHeader.setAttribute('id', 'victory');
+
+// Append the newHeader to the document body or any other container you desire
+document.body.appendChild(newHeader);
